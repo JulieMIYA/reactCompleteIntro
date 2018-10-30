98e57e52b760a7dbf4009919f2793a11f21e54f9
coucou_miya <ybb.emily@gmail.com>
2018-10-30 15:27:43 +0100

    configure eslint with react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me !</h1>
+      <Pet name="luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Dokin" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
